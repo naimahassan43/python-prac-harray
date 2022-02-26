865a74ae39a715ebcb520baef3c55e354cc06304
Naima Hassan <naima.hassan434@gmail.com>
2022-02-26 22:34:26 +0000

    Python-Ch-1: Pr-4-5-python os module

---
+++ Chapter-1/05_pr-4.py
@@ -0,0 +1,5 @@
+#4.Write a program to print the contents of a directory using OS module.Search online for the function which does that
+#5.Label the program written in Prob 4 with comments.
+
+import os       #Importing the module
+print(os.listdir())  #Function to display all the directory content
